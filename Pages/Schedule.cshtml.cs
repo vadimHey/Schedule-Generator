9fde3a9a18af368d6461b168e83feb77cda3427c
vadimHey <mr.vadem2017@gmail.com>
2025-08-16 23:20:36 +0300

    Создан минимальный генератор расписания

---
+++ Pages/Schedule.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScheduleGenerator.Pages
+{
+    public class ScheduleModel : PageModel
+    {
+        [BindProperty]
+        public string Items { get; set; } = string.Empty;
+
+        public List<string> Schedules { get; set; } = new List<string>();
+
+        public void OnPost()
+        {
+            if(!string.IsNullOrEmpty(Items))
+            {
+                Schedules = Items
+                    .Split(new[] { ',', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => s.Trim())
+                    .ToList();
+            }
+        }
+    }
+}
