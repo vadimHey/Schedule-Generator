ee75a793b681882ea798a9a136f8979f75c7c9fb
vadimHey <mr.vadem2017@gmail.com>
2025-08-18 18:55:47 +0300

    Добавлена валидация и очистка текста после ввода

--- Pages/Schedule.cshtml.cs
+++ Pages/Schedule.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace ScheduleGenerator.Pages
@@ -7,6 +8,9 @@ namespace ScheduleGenerator.Pages
     public class ScheduleModel : PageModel
     {
         [BindProperty]
+        [Required(ErrorMessage = "Введите название события")]
+        [RegularExpression(@"^[A-Za-zА-Яа-я].*", ErrorMessage = "Событие должно начинаться с буквы")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Название должно быть от 3 до 50 символов")]
         public string NewItem { get; set; } = string.Empty;
 
         public List<string> Schedules { get; set; } = new List<string>();
@@ -15,16 +19,20 @@ namespace ScheduleGenerator.Pages
         {
             LoadSchedules();
         }
+
         public IActionResult OnPost()
         {
             LoadSchedules();
 
-            if(!string.IsNullOrEmpty(NewItem))
+            if(!ModelState.IsValid)
             {
-                Schedules.Add(NewItem.Trim());
-                SaveSchedules();
+                return Page();
             }
 
+            Schedules.Add(NewItem.Trim());
+            SaveSchedules();
+
+            ModelState.Clear();
             NewItem = string.Empty;
 
             return Page();
