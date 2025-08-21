671c3bf126decee3ae10cbe9503bdfe0e47ee00b
vadimHey <mr.vadem2017@gmail.com>
2025-08-22 01:28:18 +0300

    Добавлено подключение к БД

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ScheduleGenerator.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddRazorPages();
 builder.Services.AddSession();
@@ -15,7 +21,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseSession();
 
 app.MapRazorPages();
