cd4a4f4f720e256e2bd2d45210fd1a3dee304165
vadimHey <mr.vadem2017@gmail.com>
2025-08-22 01:30:40 +0300

    Создана модель данных

---
+++ Models/ScheduleItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScheduleGenerator.Models
+{
+    public class ScheduleItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+    }
+}
