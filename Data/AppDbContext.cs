0288cc9f9f4b25a5243ae64b0ba18fc2cb0ba8e2
vadimHey <mr.vadem2017@gmail.com>
2025-08-22 01:31:09 +0300

    Создан DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleGenerator.Models;
+
+namespace ScheduleGenerator.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ScheduleItem> ScheduleItems { get; set; } 
+    }
+}
