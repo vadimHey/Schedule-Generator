91ac136c9c5b09703ce79dd21ad79536c8adb5fd
vadimHey <mr.vadem2017@gmail.com>
2025-08-25 18:12:58 +0300

    Добавлены дата и временя событий в модель

--- Models/ScheduleItem.cs
+++ Models/ScheduleItem.cs
@@ -10,5 +10,16 @@ namespace ScheduleGenerator.Models
         [Required]
         [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Today);
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeOnly StartTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeOnly EndTime { get; set; }
     }
 }
